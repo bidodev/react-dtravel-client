0f23008339fbf0843bec082e40d9fc48fe455693
Simona <see.moona@yahoo.com>
2020-07-09 15:03:03 +0200

    intro structure

--- src/components/intro/intro.component.jsx
+++ src/components/intro/intro.component.jsx
@@ -1,5 +1,32 @@
-import React from 'react';
+import React from "react";
 
-const Intro = () => (<div className="intro">Hello Intro</div>)
+const Intro = () => {
+  return (
+    <div className="intro">
+        {/* LOGO */}
+      <div className="logo">
+        <p> do the travel.</p>
+      </div>
+{/* Content part */}
+      <div className="content">
+          <h1>Get ready for your lifetime journey!</h1>
+          <h5>
+            Collection of the most beautiful places, experiences and unusual
+            housing in the world.
+          </h5>
+          <button type="button" class="btn btn-primary btn-lg">
+            Get started
+          </button>
+      </div>
+{/* Page change */}
+      <div className="pages">
 
-export default Intro
+      <ion-icon name="chevron-back-outline"></ion-icon>
+      <p>Get inspired</p>
+      <ion-icon name="chevron-forward-outline"></ion-icon>
+
+      </div>
+    </div>
+  );
+};
+export default Intro;
