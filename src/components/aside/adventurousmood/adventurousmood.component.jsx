6b5bba418023857d7839d04919bd0267949e2c1c
Claudinei Bido <bido.c@live.com>
2020-07-09 12:21:40 +0200

    Built basic bones-layout build aside basic-components moved layout files to scss and splited them

---
+++ src/components/aside/adventurousmood/adventurousmood.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdventurousMood() {
+    return (
+        <div className="aside-adventurous__mood">
+            AdventurousMood Bar
+        </div>
+    )
+}
+
+export default AdventurousMood
