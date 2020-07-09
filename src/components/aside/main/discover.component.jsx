1b1f689fc2e7f57848b2a8103f89c53bc32e778e
Claudinei Bido <bido.c@live.com>
2020-07-09 14:39:30 +0200

    my bad

--- src/components/aside/main/discover.component.jsx
+++ src/components/aside/main/discover.component.jsx
@@ -1,18 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 
-function Main() {
+const ExperienceItem = ({ title }) => {
+  return <h1>{title}</h1>;
+};
+const Experiences = () => {
+  const fakeDB = [{ title: "Juca" }, { title: "Jose" }, { title: "Pedro" }];
+
+  return (
+    <div>
+      {fakeDB.map((exp) => (
+        <ExperienceItem {...exp} />
+      ))}
+      {/* generate the list of experiences */}
+    </div>
+  );
+};
+const Places = () => <div>Hello Places</div>;
+const Housing = () => <div>Hello Housing</div>;
+
+const Main = () => {
   return (
     <div className="aside-main">
       <h1>Discover</h1>
       <nav className="aside-main__nav">
         <Link to="/experiences">Experiences</Link>
         <Link to="/places">Places</Link>
-        <Link to="/houses">Housings</Link>
+        <Link to="/housings">Housings</Link>
       </nav>
-      <div className="aside-main__carrousel">Carrousel</div>
+
+      <div className="aside-main__carrousel">
+        <Switch>
+          <Route exact path="/experiences" component={Experiences} />
+          <Route exact path="/places" component={Places} />
+          <Route exact path="/housings" component={Housing} />
+        </Switch>
+      </div>
     </div>
   );
-}
+};
 
 export default Main;
