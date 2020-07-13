9be2f2710f72d9eb4bb9cd58852608e57aee4a89
Claudinei Bido <bido.c@live.com>
2020-07-13 15:03:16 +0200

    suggestion item

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import loginReducer from './login-reducer'
 import dataReducer from './data-reducer'
+import searchReducer from './search-redux'
 
 const rootReducer = combineReducers({
     login: loginReducer,
-    destinations: dataReducer
+    destinations: dataReducer,
+    searchInput: searchReducer
 })
 
 export default rootReducer;
