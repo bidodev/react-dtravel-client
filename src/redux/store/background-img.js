490006184093a23439e9aed3f0d31e2d29f3bdb6
Simona <see.moona@yahoo.com>
2020-07-14 09:38:54 +0200

    background db

---
+++ src/redux/store/background-img.js
@@ -0,0 +1,10 @@
+[
+  "./img/img-background/photo0.jpg",
+  "./img/img-background/photo1.jpg",
+  "./img/img-background/photo2.jpg",
+  "./img/img-background/photo3.jpg",
+  "./img/img-background/photo4.jpg",
+  "./img/img-background/photo5.jpg",
+  "./img/img-background/photo6.jpg",
+  "./img/img-background/photo7.jpg",
+];
