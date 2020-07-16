4ea9d7ba61f2ee6018293a1ece852e3cdc5294ca
hadabr <alex.dovghii@gmail.com>
2020-07-16 09:21:08 +0200

    some bugfix

--- src/components/intro/quiz/question-item.component.js
+++ src/components/intro/quiz/question-item.component.js
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import Btn from "./btn.component"
 
 const inputObj = {  }
-const QuestionItem = forwardRef(({id, question, variants}, ref) => {
+const QuestionItem = forwardRef(({id, question, variants, style}, ref) => {
   const dispatch = useDispatch();
   const onSubmit = (event) => {
     event.preventDefault();
@@ -21,7 +21,7 @@ const QuestionItem = forwardRef(({id, question, variants}, ref) => {
   />
     ));
     return (
-        <div id={id} ref={ref} className = "quiz-item">
+        <div id={id} ref={ref} className = "quiz-item" style = {style}>
         <h3>{question}</h3>
         {btns}
     </div>
