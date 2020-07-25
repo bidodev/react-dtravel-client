712d0ae60f0fbab1cdcae62a905a7dcf62fb6f01
Claudinei Bido <bido.c@live.com>
2020-07-25 13:42:30 +0200

    layout fixs

--- src/components/intro/landing/landing.component.js
+++ src/components/intro/landing/landing.component.js
@@ -10,26 +10,45 @@ import { Carousel } from "react-responsive-carousel";
 const Landing = () => {
   const backgrounds = useSelector(({ data }) => data.backgrounds);
 
+  const getConfigurableProps = {
+    showArrows: true,
+    showStatus: false,
+    showIndicators: true,
+    infiniteLoop: false,
+    showThumbs: false,
+    useKeyboardArrows: true,
+    autoPlay: true,
+    stopOnHover: true,
+    swipeable: true,
+    dynamicHeight: true,
+    emulateTouch: true,
+    thumbWidth: 100,
+    selectedItem: 0,
+    interval: 10000,
+    transitionTime: 400,
+    swipeScrollTolerance: 5,
+  };
+
   return (
     <div className="landing-wrapper">
-      <Carousel showThumbs={false}>
+      <Carousel {...getConfigurableProps}>
         {backgrounds.map((background) => (
           <div>
             <img src={`${background}`} alt={`${background}`} />
-            <p className="legend">Legend 1</p>
+            {/* <p className="legend">Legend</p> */}
           </div>
         ))}
       </Carousel>
-      <div className="logo">
-        <p>
-          dtravel<span>.</span>
-        </p>
-      </div>
+
       <div className="content">
+        <div className="logo">
+          <p>
+            dtravel<span>.</span>
+          </p>
+        </div>
         <h1>Get ready for your lifetime journey!</h1>
         <h5>
-          Collection of the most beautiful places, experiences and unusual
-          housing in the world
+          Collection of the most beautiful places, experiences and unusual housing in the world
         </h5>
         <Link to="/quiz">
           <button type="button" className="btn btn-primary btn-lg">
