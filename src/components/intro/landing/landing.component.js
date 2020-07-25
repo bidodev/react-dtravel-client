1a50b058468aebe80438d20497964e5b48a0919b
Claudinei Bido <bido.c@live.com>
2020-07-25 13:00:50 +0200

    kinda of working

--- src/components/intro/landing/landing.component.js
+++ src/components/intro/landing/landing.component.js
@@ -1,60 +1,41 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 import "./landing.title.styles.scss";
-//import smoothScroll from "./smoothScroll";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
+import { Carousel } from "react-responsive-carousel";
 
 const Landing = () => {
-  const [img, setImg] = useState(0);
-
-  const Urls = useSelector(({data}) => data.backgrounds);
-
-  const handleImg = () => {
-    const arrayItems = Urls.length - 1;
-    let numberImg = 0;
-
-    if (img >= (arrayItems)) {
-      setImg(0);
-    } else {
-      numberImg = img + 1;
-      setImg(numberImg);
-    }
-    
-  };
-
-  let imgStyle = {
-    backgroundImage: `linear-gradient(to left bottom,
-            rgba(0, 0, 0, 0.1),
-            rgba(0, 0, 0, 0.7)
-          ), url("${Urls[img]}")`,
-  };
+  const backgrounds = useSelector(({ data }) => data.backgrounds);
 
   return (
-    <div className="main-title" style={imgStyle}>
-      {/* LOGO */}
+    <div className="landing-wrapper">
+      <Carousel showThumbs={false}>
+        {backgrounds.map((background) => (
+          <div>
+            <img src={`${background}`} alt={`${background}`} />
+            <p className="legend">Legend 1</p>
+          </div>
+        ))}
+      </Carousel>
       <div className="logo">
-        <p>dtravel<span>.</span></p>
+        <p>
+          dtravel<span>.</span>
+        </p>
       </div>
-      {/* Content part */}
       <div className="content">
         <h1>Get ready for your lifetime journey!</h1>
         <h5>
-          Collection of the most beautiful places, experiences and unusual housing in the world
+          Collection of the most beautiful places, experiences and unusual
+          housing in the world
         </h5>
         <Link to="/quiz">
           <button type="button" className="btn btn-primary btn-lg">
             Get Started
           </button>
         </Link>
-      </div>
-
-      {/* onClick make the URL inside imgStyle change, you can pass it with props */}
-      <div className="pagination">
-        <ion-icon name="chevron-back-outline" onClick={handleImg}></ion-icon>
-        <p>Get inspired</p>
-        <ion-icon name="chevron-forward-outline" onClick={handleImg}></ion-icon>
       </div>
     </div>
   );
