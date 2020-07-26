7c00bf545bad542e4bdfb3604cb59a58fcc3a19a
Claudinei Bido <bido.c@live.com>
2020-07-26 15:48:35 +0200

    ....

--- src/pages/favorites.jsx
+++ src/pages/favorites.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import './favorites.styles.scss'
 
-import {useSelector} from "react-redux";
+import ExperienceItem from "../components/aside/discover/experiences/experience.item.component";
+
+import { useSelector } from "react-redux";
+
+const openModal = () => {
+    
+}
 
 function Favorites() {
     const favoritesList = useSelector((state) => state.favoritesList)
+    const data = useSelector((state) => state.data.destinations)
+
+    
+    console.log(favoritesList, data)
+
 
     return (
         <div className="wrapper">
             <h2>Your Favorites</h2>
-            {favoritesList.map(({id, name}) => (<li key={id}>{name}</li>))}
+            {/* {favoritesList.map(({ ...item }) => (
+            <ExperienceItem key={item.id} {...item} openModal={openModal} />
+        ))} */}
         </div>
     )
 }
